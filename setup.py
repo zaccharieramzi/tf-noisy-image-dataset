7dc63af645e0de9168c99f8e7cb27a02e695f736
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-01 15:31:24 +0100

    increased version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as open_file:
 
 setuptools.setup(
     name="tf-noisy-image-dataset",
-    version="0.0.1",
+    version="0.0.2",
     author="Zaccharie Ramzi",
     author_email="zaccharie.ramzi@gmail.com",
     description="Datasets for noisy images in the tf.data API.",
